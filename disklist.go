b654e5cc87d76e235456b651b19d97fa5f3c7a3e
Jon Barber <jon.barber@acm.org>
2021-03-21 15:30:46 +0000

    Refactor : calculate the file path in one place

--- disklist.go
+++ disklist.go
@@ -31,23 +31,29 @@ func (dl *DiskList) Items() []Todo {
 	return dl.list.Items()
 }
 
+func (dl *DiskList) filename() string {
+	return path.Join(dl.dir, "todo.json")
+}
+
+// store writes the contents of the list to disk
 func (dl *DiskList) store() error {
 	contents, err := json.MarshalIndent(dl.Items(), "", " ")
 
 	if err != nil {
 		return fmt.Errorf("unable to prepare list to store to disk : %v", err)
 	}
-	if err = ioutil.WriteFile(path.Join(dl.dir, "todo.json"), contents, 0644); err != nil {
+	if err = ioutil.WriteFile(dl.filename(), contents, 0644); err != nil {
 		return fmt.Errorf("unable to store list to disk : %v", err)
 	}
 
 	return nil
 }
 
+// restore reads any previously stored list from disk
 func (dl *DiskList) restore() error {
-	if _, err := os.Stat(path.Join(dl.dir, "todo.json")); err != nil {
+	if _, err := os.Stat(dl.filename()); err != nil {
 		if os.IsNotExist(err) {
-			return nil
+			return nil // assume this means this is a brand new list - no stored file
 		}
 		return fmt.Errorf("unable to restore list from disk : %v", err)
 	}
@@ -55,7 +61,7 @@ func (dl *DiskList) restore() error {
 }
 
 func (dl *DiskList) read() error {
-	content, err := ioutil.ReadFile(path.Join(dl.dir, "todo.json"))
+	content, err := ioutil.ReadFile(dl.filename())
 	if err != nil {
 		return fmt.Errorf("unable to read list from disk : %v", err)
 	}
